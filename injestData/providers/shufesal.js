bf39e488f2bbea6f7af7f85cbb7f5c19dd25f368
hagai <hagay.maya@gmail.com>
2022-01-08 00:52:08 +0200

    xml Data

--- injestData/providers/shufesal.js
+++ injestData/providers/shufesal.js
@@ -3,72 +3,95 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const axios = require('axios');
 const fs = require('fs');
-const Path = require('path'); 
+const Path = require('path');
+const zlib = require("zlib");
 
-const getFileDownloadLinksFromPage = async (pagenumber)=> {
+const getFileDownloadLinksFromPage = async (pagenumber) => {
     let data = await JSDOM.fromURL("http://prices.shufersal.co.il/FileObject/UpdateCategory?catID=2&storeId=0&page=" + pagenumber);
     let links = [...data.window.document.querySelectorAll(".webgrid-row-style")]
-                    .map((item) => 
-                        {
-                                return item.querySelector("a").href
-                        });
+        .map((item) => {
+            return item.querySelector("a").href
+        });
 
-   
+
     return links;
 }
-const getNumberOfPages = async ()  => {
+const getNumberOfPages = async () => {
     let data = await JSDOM.fromURL("http://prices.shufersal.co.il/");
-    let numberOfPages =0;
+    let numberOfPages = 0;
     [...data.window.document.querySelector(".webgrid-footer").querySelectorAll("a")].map(
-        (item) => { 
-           return item.href.toString().replace("http://prices.shufersal.co.il/?page=", "");
+        (item) => {
+            return item.href.toString().replace("http://prices.shufersal.co.il/?page=", "");
         }).forEach((pageNumber) => {
-            if(pageNumber > numberOfPages){
+            if (pageNumber > numberOfPages) {
                 numberOfPages = pageNumber;
             }
         })
-    
+
     return numberOfPages;
-    
+
 }
 
-const downloadGzFile = async (gzFile)=> {
-    const dirPath = Path.resolve(__dirname + "../../../data/shufersal/gz/", gzFile.branchId); 
-    
+const downloadGzFile = async (gzFile) => {
+    const dirPath = Path.resolve(__dirname + "../../../data/shufersal/gz/", gzFile.branchId);
+
     const path = Path.resolve(dirPath, gzFile.name);
-    console.log(path);
     //const writer = fs.createWriteStream(path);
     const response = await axios({
         url: gzFile.url,
         method: 'GET',
-        responseType: 'gzip'
-      });
+        responseType: 'stream'
+    });
 
-    fs.promises.mkdir(dirPath, { recursive: true })
-        .then(() => { return fs.promises.writeFile(path, response.data);})
-        .catch((error) => {console.log("mkdirerror", "error")})
+    await fs.promises.mkdir(dirPath, { recursive: true })
+        .catch((error) => { console.log("mkdirerror", "error") })
+
+    const file = fs.createWriteStream(path);
+
+    //await response.data.pipe(file);
+
+    gzFile.path = path;
+    gzFile.XMLpath = Path.resolve(dirPath, gzFile.name.replace("gz","xml"));
+    const writeStream = fs.createWriteStream(gzFile.XMLpath);
+    const unzip = zlib.createGunzip();
+
+    await response.data.pipe(unzip).pipe(writeStream);
+    return gzFile;
+}
+
+const unZipGzFile = async (gzFile) => {
+    const fileContents = fs.createReadStream(gzFile.path);
+    const writeStream = fs.createWriteStream(gzFile.path.replace("gz","xml"));
+    const unzip = zlib.createGunzip();
+
+    fileContents.pipe(unzip).pipe(writeStream);
+        
 }
 const main = async () => {
     let numberOfPages = await getNumberOfPages();
-    for (let pageNumber =1; pageNumber <= numberOfPages; pageNumber++ ){
+    for (let pageNumber = 1; pageNumber <= numberOfPages; pageNumber++) {
         getFileDownloadLinksFromPage(pageNumber).then(
             async (urls) => {
-                    urls.forEach((gzLink) => {
-                        const url = new URL(gzLink);
-                        const remotefileName = url.pathname.replace("/pricefull/", "");
-                        const branchId = url.pathname.split("-")[1];
-                        let gzFile = {
-                            name: remotefileName,
-                            branchId: branchId,
-                            url: url.href
-                        };
-                        downloadGzFile(gzFile).catch((err)=> console.log(err));
-                    });
-                    
-                
-        });
-                          
-    }  
+                urls.forEach((gzLink) => {
+                    const url = new URL(gzLink);
+                    const remotefileName = url.pathname.replace("/pricefull/", "");
+                    const branchId = url.pathname.split("-")[1];
+                    let gzFile = {
+                        name: remotefileName,
+                        branchId: branchId,
+                        url: url.href
+                    };
+                    downloadGzFile(gzFile)
+                        .then((gzFile) => {
+                            console.log(gzFile);
+                         //  unZipGzFile(gzFile).catch((err)=> {console.log("ERR: ", err)});
+                        });
+                });
+
+
+            });
+
+    }
 }
 
 main();
