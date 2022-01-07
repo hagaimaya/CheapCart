ecbfea7629949f9c82902ac9da96e46f2ff44ee6
hagai <hagay.maya@gmail.com>
2022-01-07 23:09:22 +0200

    working downloading gz file

--- injestData/providers/shufesal.js
+++ injestData/providers/shufesal.js
@@ -2,7 +2,8 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const axios = require('axios');
-
+const fs = require('fs');
+const Path = require('path'); 
 
 const getFileDownloadLinksFromPage = async (pagenumber)=> {
     let data = await JSDOM.fromURL("http://prices.shufersal.co.il/FileObject/UpdateCategory?catID=2&storeId=0&page=" + pagenumber);
@@ -31,26 +32,43 @@ const getNumberOfPages = async ()  => {
     
 }
 
-const downloadGzFile = async (gzUrl)=> {
-    axios({
-        method: 'get',
-        url: gzUrl,
-        responseType: 'stream'
-      })
+const downloadGzFile = async (gzFile)=> {
+    const dirPath = Path.resolve(__dirname + "../../../data/shufersal/gz/", gzFile.branchId); 
+    
+    const path = Path.resolve(dirPath, gzFile.name);
+    console.log(path);
+    //const writer = fs.createWriteStream(path);
+    const response = await axios({
+        url: gzFile.url,
+        method: 'GET',
+        responseType: 'gzip'
+      });
+
+    fs.promises.mkdir(dirPath, { recursive: true })
+        .then(() => { return fs.promises.writeFile(path, response.data);})
+        .catch((error) => {console.log("mkdirerror", "error")})
 }
 const main = async () => {
     let numberOfPages = await getNumberOfPages();
-    let urls =[];
     for (let pageNumber =1; pageNumber <= numberOfPages; pageNumber++ ){
-        urls.push((await getFileDownloadLinksFromPage(pageNumber))
-                            .map((item) => {
-                                    return item}));
-    }
-    
-   console.log(urls);
-    //gunzip('example.json.gz', 'example.json');
-    
-    //urls[0];   
+        getFileDownloadLinksFromPage(pageNumber).then(
+            async (urls) => {
+                    urls.forEach((gzLink) => {
+                        const url = new URL(gzLink);
+                        const remotefileName = url.pathname.replace("/pricefull/", "");
+                        const branchId = url.pathname.split("-")[1];
+                        let gzFile = {
+                            name: remotefileName,
+                            branchId: branchId,
+                            url: url.href
+                        };
+                        downloadGzFile(gzFile).catch((err)=> console.log(err));
+                    });
+                    
+                
+        });
+                          
+    }  
 }
 
 main();
